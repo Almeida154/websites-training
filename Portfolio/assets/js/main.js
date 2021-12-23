541b17994cbd70b19d3200b3313b29c52843134c
Almeida154 <davidalmeida154of@gmail.com>
2021-12-23 00:25:05 -0300

    :construction: Portfolio Website | Navbar

--- Portfolio/assets/js/main.js
+++ Portfolio/assets/js/main.js
@@ -1,12 +1,27 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
-/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
 /*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLinks = document.querySelectorAll(".nav__link");
+navLinks.forEach((link) =>
+  link.addEventListener("click", () => navMenu.classList.remove("show-menu"))
+);
 
 /*==================== ACCORDION SKILLS ====================*/
 
